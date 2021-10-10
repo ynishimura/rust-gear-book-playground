ca390264def6a7a95cd732f7f44aabce0273418a
ynishimura <ynishimura0922@gmail.com>
2021-10-10 23:04:59 +0900

    add first

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
     println!("Hello, world!");
+    let objective: Option<i32> = Some(1);
+    match objective {
+        Some(x) if x % 2 == 0 => println!("偶数です: {}", x),
+        Some(x) => println!("奇数です: {}", x),
+        None => println!("値がありません"),
+    }
 }
