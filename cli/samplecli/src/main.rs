0e05dca3a6122f82aea23c30bf4da297c8261f9a
ynishimura <ynishimura0922@gmail.com>
2021-10-21 20:17:48 +0900

    clap v2

--- cli/samplecli/src/main.rs
+++ cli/samplecli/src/main.rs
@@ -1,32 +1,31 @@
-use clap::{App, Arg};
+use clap::{AppSettings, Parser};
 
+#[derive(Parser,Debug)]
+#[clap(
+    name = "My RPN probram",
+    version = "1.0.0",
+    author = "Your name",
+    about = "Super awesome sample RPN calculator"
+)]
+struct Opts {
+    // set the level of verbosity
+    #[clap(short,long)]
+    verbose: bool,
+
+    // // Number
+    // #[clap(name = "NUMBER")]
+    // num: i32,
+
+    // Formulas written in RPN
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
 fn main() {
-    // let args: Vec<String> = env::args().collect();
-    // println!("{:?}", args);
-    let matches = App::new("My RPN program")
-        .version("1.0.0")
-        .author("Your name")
-        .about("Super awesome sample RPN calculator")
-        .arg(
-            Arg::new("formula_file")
-                .about("Formulas written in RPN")
-                .value_name("FILE")
-                .index(1)
-                .required(false),
-        )
-        .arg(
-            Arg::new("verbose")
-                .about("Sets the level of verbosity")
-                .short('v')
-                .long("verbose")
-                .required(false),
-        )
-        .get_matches();
-    match matches.value_of("formula_file") {
+    let opts = Opts::parse();
+    match opts.formula_file {
         Some(file) => println!("File specified: {}", file),
         None => println!("No file specified."),
     }
-    let verbose = matches.is_present("verbose");
-    println!("Is verbosity specified?: {}", verbose);
+    println!("Is verbosity specified?: {}", opts.verbose);
 
 }
