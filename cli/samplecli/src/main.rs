8b73780f4e41d00de2c476d7ac846f4406d0b677
ynishimura <ynishimura0922@gmail.com>
2021-10-24 18:52:39 +0900

    add calc logic

--- cli/samplecli/src/main.rs
+++ cli/samplecli/src/main.rs
@@ -2,6 +2,51 @@ use clap::Parser;
 use std::fs::File;
 use std::io::{stdin, BufRead, BufReader};
 
+//　今回はオプションが一つしかないので、無名で作成
+struct RpnCalculator(bool);
+
+impl RpnCalculator {
+    pub fn new(verbose:bool) -> Self {
+        Self(verbose)
+    }
+
+    pub fn eval(&self, formula: &str) -> i32 {
+        let mut tokens = formula.split_whitespace().rev().collect::<Vec<_>>();
+        self.eval_inner(&mut tokens)
+    }
+
+    fn eval_inner(&self, tokens: &mut Vec<&str>) -> i32 {
+        let mut stack = Vec::new();
+
+        while let Some(token) = tokens.pop() {
+            if let Ok(x) = token.parse::<i32>() {
+                stack.push(x);
+            } else {
+                let y = stack.pop().expect("invalid syntax");
+                let x = stack.pop().expect("invalid syntax");
+                let res = match token {
+                    "+" => x + y,
+                    "-" => x - y,
+                    "*" => x * y,
+                    "/" => x / y,
+                    "%" => x % y,
+                    _ => panic!("invalid token"),
+                };
+                stack.push(res);
+            }
+
+            if self.0 {
+                println!("{:?} {:?}", tokens, stack);
+            }
+        }
+        if stack.len() == 1 {
+            stack[0]
+        } else {
+            panic!("invalid syntax");
+        }
+    }
+}
+
 #[derive(Parser,Debug)]
 #[clap(
     name = "My RPN probram",
@@ -42,8 +87,11 @@ fn main() {
 }
 
 fn run<Reader: BufRead>(reader: Reader, verbose:bool) {
+    let calc = RpnCalculator::new(verbose);
+
     for line in reader.lines() {
         let line = line.unwrap();
-        println!("{}", line);
+        let answer = calc.eval(&line);
+        println!("{}", answer);
     }
 }
